650e8558e91924bf7a65fee2aaefe5dd6ae78ea6
Serge3452 <127433233+Serge3452@users.noreply.github.com>
2023-12-18 22:05:26 +0500

    Add files via upload
    Урок №5

---
+++ lesson_5_py.py
@@ -0,0 +1,68 @@
+
+# for i in range (1,10):
+#     if i==4:
+#         break
+#     print (i)
+
+# lst=['Name',"LastNane",'Age']
+
+# for i in lst:
+#     print(f'89999999: {i}')
+    
+# lst = "Serge"    
+# for i in lst:
+#         print (i)
+#a = ""
+
+# for i in "Timur":
+#     a = i +a
+# print (a)
+
+# for i in [1,2,3,4,6]:
+#     if i ==  5:
+#         print (i)
+#         break
+# else:
+#         print ('No')
+
+
+# for i in range(5,0,-1):
+#     print(i)
+
+
+# for i in [1, 2, 3, 4, 5,6]:
+#     if i%2==0:
+#         print (i)
+    
+# # res = ['P', 'y', 't', 'h', 'o', 'n']
+# #     for i in res
+# #     print 
+# a =''
+# for i in ['P', 'y', 't', 'h', 'o', 'n']:
+#     a += i
+# print(a)
+
+
+# Домашнее задание №1
+#Через цикл for посчитать сумму всех чисел, которые вы вводите с клавиатуры
+# самостоятельно (например, от 13 до числа, которое вводите самостоятельно).
+a=13
+b=int (input ('Ввведите второе число  '  ))
+# b=abs(b-a)
+sum=0
+for i in range(a,b):
+    sum+=i
+    print(sum)
+    
+#   Домашнее задание №2
+# Вывести четные числа из диапазона от 0 до числа, которое вы вводите с клавиатуры.
+# a=int(input ('Ввведите число'))+1
+# for i in range (1,a):
+#     if i % 2==0:
+#         print (i)
+        
+        
+  
+
+        
+    
